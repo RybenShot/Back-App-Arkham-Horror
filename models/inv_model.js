f95925fff85dd6c87cb9a1c146768a574c4414b0
sebastian <sebastianjimenezroman@hotmail.com>
2025-04-25 13:25:48 +0200

    se deja todo listo para PROD, nuevos crud de investigadores, sus previos y sus objetos iniciales.

--- models/inv_model.js
+++ models/inv_model.js
@@ -2,6 +2,7 @@ import invListJSON from '../databaseJSON/investigadores.json' with { type: "json
 import previewInvListJSON from '../databaseJSON/previewInv.json' with { type: "json" }
 import { randomUUID } from 'node:crypto'
 import { writeFileSync } from 'fs'
+import { ObjectModel } from '../models/object_model.js';
 
 
 export class InvModel{
@@ -22,8 +23,9 @@ export class InvModel{
     }
 
     static async getAllPreview({ rol }){
+        const activos = previewInvListJSON.filter(inv => inv.isActive);
         if (rol) {
-            const filterRol = previewInvListJSON.filter (
+            const filterRol = activos.filter (
                 
                 previewInv => previewInv.rol.some(
                     tipo => tipo.toLowerCase() === rol.toLowerCase()
@@ -31,11 +33,38 @@ export class InvModel{
             )
             return filterRol
         }
-        return previewInvListJSON
+        return activos
+    }
+
+    static async getInvObjects({ id }) {
+        const inv = invListJSON.find(inv => inv.idInv == id);
+        if (!inv || !inv.possessions) {
+          return {
+            objects: [],
+            optionalText: '',
+            optionalObjects: []
+          };
+        }
+      
+        const { required = [], optional = [], optionalText = '' } = inv.possessions;
+      
+        const objects = await Promise.all(
+          required.map(id => ObjectModel.getByID({ id }))
+        );
+      
+        const optionalObjects = await Promise.all(
+          optional.map(id => ObjectModel.getByID({ id }))
+        );
+      
+        return {
+          objects,
+          optionalText,
+          optionalObjects
+        };
     }
 
     static async getByID ({id}){
-        const inv = invListJSON.find(inv => inv.id == id)
+        const inv = invListJSON.find(inv => inv.idInv == id)
         return inv
     }
 
